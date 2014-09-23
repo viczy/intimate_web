8ab89d31493e21476cdc78795ff3f3f8c6d2c65c
Zhouhaijian <viczy@ymail.com>
2014-09-23 15:38:58 +0800

    update the signin signup view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-sclass ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception #预防CSRF（跨站请求伪造）。
