5938667856737005dff67e00ca5be0f20e78d0aa
Zhouhaijian <viczy@ymail.com>
2014-10-29 18:34:58 +0800

    add the requirjs,but has bug

---
+++ vendor/assets/javascripts/samsung_grid.js
@@ -0,0 +1,20 @@
+//main.js
+
+alert("加载成功！");
+
+// require.config({
+// 	baseUrl: "/assets/",
+// 	paths: {
+// 		"masonry": "masonry.pkgd.min",
+// 		"imagesloaded": "imagesloaded.pkgd.min",
+// 		"classie": "classie",
+// 		"colorfinder": "colorfinder-1.1",
+// 		"gridscrollfx": "gridScrollFx"
+// 	}
+// });
+
+// require(['masonry', 'imagesloaded', 'classie', 'gridscrollfx'], function (){
+// 　　new GridScrollFx(document.getElementById('grid'), {
+// 		viewportFactor : 0.4
+// 	});
+// });
