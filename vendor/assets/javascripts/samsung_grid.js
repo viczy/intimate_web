f590708095f6cd70063cce7bb3dd88da60ab8675
Zhouhaijian <viczy@ymail.com>
2014-10-30 17:40:51 +0800

    review code and fix the grid bug

--- vendor/assets/javascripts/samsung_grid.js
+++ vendor/assets/javascripts/samsung_grid.js
@@ -1,20 +1,6 @@
-//main.js
-
-alert("加载成功！");
-
-// require.config({
-// 	baseUrl: "/assets/",
-// 	paths: {
-// 		"masonry": "masonry.pkgd.min",
-// 		"imagesloaded": "imagesloaded.pkgd.min",
-// 		"classie": "classie",
-// 		"colorfinder": "colorfinder-1.1",
-// 		"gridscrollfx": "gridScrollFx"
-// 	}
-// });
-
-// require(['masonry', 'imagesloaded', 'classie', 'gridscrollfx'], function (){
-// 　　new GridScrollFx(document.getElementById('grid'), {
-// 		viewportFactor : 0.4
-// 	});
-// });
+//= require masonry.pkgd.min.js
+//= require imagesloaded.pkgd.min.js
+//= require classie.js
+//= require colorfinder-1.1.js
+//= require gridScrollFx.js
+//= require samsung_grid.js
