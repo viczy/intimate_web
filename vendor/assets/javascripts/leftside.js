5a26c56e09b3a0e063dbfe203f33c99ae0ec3ecd
Zhouhaijian <viczy@ymail.com>
2014-11-12 17:33:05 +0800

    update the leftside js

---
+++ vendor/assets/javascripts/leftside.js
@@ -0,0 +1,43 @@
+;( function( window ) {
+	
+	'use strict';
+
+	var leftsideUL;
+    var all;
+
+    if (window.currentIndex === undefined) {
+    	window.currentIndex = 12;
+    };
+    if (!leftsideUL) {
+    	leftsideUL = $('#leftside');
+    };
+
+    if (!all) {
+    	all= leftsideUL.children('li');
+    };
+
+    all.click(function(){
+        leftsideUL.class= 'leftside';
+        leftsideUL.addClass($(this).attr('class'));
+    });
+
+    all.hover(
+        function () {
+            $('a',$(this)).stop().animate({'marginLeft':'-2px'},200);
+        },
+        function () {
+            $('a',$(this)).stop().animate({'marginLeft':'-75px'},200);
+        }
+    );
+
+    all.each(function(){
+        var indexA = $(this);
+        indexA.removeClass("tab_current");
+        if (leftsideUL.hasClass(indexA.attr('class'))) {
+            indexA.addClass("tab_current");
+        };
+    });
+
+    $('#leftside a').stop().animate({'marginLeft':'-75px'},1000);
+
+})( window );
