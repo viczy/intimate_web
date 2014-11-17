534058bac5b58dd78e712113a550078f44816db9
Zhouhaijian <viczy@ymail.com>
2014-11-17 17:51:17 +0800

    add the page to plans

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,7 @@
 class PlansController < ApplicationController
   def index
+  	@plans = Plan.all.page(params[:page]) 
+  	# @plans = Plan.all.page({page: @page, per_page: 20}) 
   end
 
   def new
@@ -7,9 +9,39 @@ class PlansController < ApplicationController
   end
 
   def create
+  	@plan = Plan.new(plan_params)
+    respond_to do |format|
+      format.html {
+        if @plan.save
+          flash[:success] = "Welcome to the INTIMATE!"
+          redirect_to plans_path
+        else
+          render 'new'
+        end
+      }
+
+      format.json { 
+        if @plan.save
+          render json: @plan, status: :created
+        else
+          render json: @plan.errors, status: :unprocessable_entity
+        end
+      }
+    end
 
   end
 
   def destroy
   end
+
+
+
+	private
+	    def plan_params
+	      params.require(:plan).permit(:titil, :detail)
+	    end
+
 end
+
+
+
