e7c6a44b96cba4df2f1b0c2f9d123ccc6cdb73ce
Zhouhaijian <viczy@ymail.com>
2014-11-19 12:06:42 +0800

    add the bill index and show

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,11 +1,52 @@
 class BillsController < ApplicationController
+
   def index
+  	@bills = Bill.all.page(params[:page]) 
   end
 
   def new
   	@bill = Bill.new
   end
 
-  def destroy
+  def show
+  	@bill = Bill.find(params[:id])
+    respond_to do |format|
+      format.html 
+      format.json { render :json => { id: @bill.id, createtime: @bill.createtime, sepnd: @bill.spend, detail: @bill.detail }.to_json }
+    end
+
   end
+
+  def create
+  	@bill = Bill.new(bill_params)
+    respond_to do |format|
+      format.html {
+      	if @bill.save
+          redirect_to bills_path
+        else
+          render 'new'
+        end
+      }
+
+      format.json { 
+        if @bill.save
+          render json: @bill, status: :created
+        else
+          render json: @bill.errors, status: :unprocessable_entity
+        end
+      }
+    end
+
+  end
+
+  def destroy
+
+  end
+
+
+  private
+	    def bill_params
+	      params.require(:bill).permit(:createtime, :spend, :detail)
+	    end
+
 end
--- app/controllers/reminds_controller.rb
+++ app/controllers/reminds_controller.rb
@@ -1,11 +1,53 @@
 class RemindsController < ApplicationController
+
   def index
+  	@reminds = Remind.all.page(params[:page]) 
   end
 
   def new
   	@remind = Remind.new
   end
 
-  def destroy
+  def show
+  	@remind = Remind.find(params[:id])
+    respond_to do |format|
+      format.html 
+      format.json { render :json => { id: @remind.id, alarm: @remind.alarm, address: @remind.address, content: @remind.content }.to_json }
+    end
+
   end
+
+  def create
+  	@remind = Remind.new(remind_params)
+    respond_to do |format|
+      format.html {
+      	if @remind.save
+          redirect_to reminds_path
+        else
+          render 'new'
+        end
+      }
+
+      format.json { 
+        if @remind.save
+          render json: @remind, status: :created
+        else
+          render json: @remind.errors, status: :unprocessable_entity
+        end
+      }
+    end
+  end
+
+
+  def destroy
+
+  end
+
+
+  private
+	    def remind_params
+	      params.require(:remind).permit(:alarm, :content)
+	    end
+
+
 end
